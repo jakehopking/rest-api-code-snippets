c74df40c341359b46294b7f08f433855ffc494ab
Jake Hopking <jake@hopking.io>
2020-04-26 23:26:56 +0100

    Add token auth routes

--- rest_api/snippets/urls.py
+++ rest_api/snippets/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from snippets import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -9,5 +14,8 @@ router.register(r'users', views.UserViewSet)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/', include(router.urls)),
 ]
